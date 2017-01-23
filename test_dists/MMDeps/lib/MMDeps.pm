a02b03353e311f2a469916f92a4307ec547189ad
Jon Gentle <atrodo@atrodo.org>
2017-01-22 21:31:52 -0500

    Remove the useless documentation left in the test dists

--- test_dists/MMDeps/lib/MMDeps.pm
+++ test_dists/MMDeps/lib/MMDeps.pm
@@ -6,35 +6,4 @@ use 5.008_005;
 our $VERSION = '0.01';
 
 1;
-__END__
-
-=encoding utf-8
-
-=head1 NAME
-
-MMDeps - Blah blah blah
-
-=head1 SYNOPSIS
-
-  use MMDeps;
-
-=head1 DESCRIPTION
-
-MMDeps is
-
-=head1 AUTHOR
-
-Jon Gentle E<lt>cpan@atrodo.orgE<gt>
-
-=head1 COPYRIGHT
-
-Copyright 2016- Jon Gentle
-
-=head1 LICENSE
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=head1 SEE ALSO
-
-=cut
+# ABSTRACT: Blah blah blah
--- test_dists/NoMeta/lib/NoMeta.pm
+++ test_dists/NoMeta/lib/NoMeta.pm
@@ -6,35 +6,4 @@ use 5.008_005;
 our $VERSION = '0.01';
 
 1;
-__END__
-
-=encoding utf-8
-
-=head1 NAME
-
-NoMeta - Blah blah blah
-
-=head1 SYNOPSIS
-
-  use NoMeta;
-
-=head1 DESCRIPTION
-
-NoMeta is
-
-=head1 AUTHOR
-
-Jon Gentle E<lt>cpan@atrodo.orgE<gt>
-
-=head1 COPYRIGHT
-
-Copyright 2016- Jon Gentle
-
-=head1 LICENSE
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=head1 SEE ALSO
-
-=cut
+# ABSTRACT: Blah blah blah
