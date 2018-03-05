60dd31f62161df6fd81ca35de7ea8c931c2d35d1
Jon Gentle <atrodo@atrodo.org>
2018-03-04 23:50:18 -0500

    Fix a bug where a perl-like number in the path could trip us up

--- t/helper.pm
+++ t/helper.pm
@@ -7,7 +7,7 @@ $App::MechaCPAN::LOG_ON = 0;
 $App::MechaCPAN::TIMEOUT = 0;
 
 # Delete PERL_USE_UNSAFE_INC, it will interfere with our tests.
-# This shouldn't be a problem for us since this helper has already benn
+# This shouldn't be a problem for us since this helper has already been
 # included, this is so we can test the rest of our functionality
 delete $ENV{PERL_USE_UNSAFE_INC};
 
